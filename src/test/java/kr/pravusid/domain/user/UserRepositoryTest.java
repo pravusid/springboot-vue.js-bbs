cc63897ce7a8d4d66e0f2af57b9be26f24018c42
Sangdon Park <pravusid@gmail.com>
2018-10-03 02:05:01 +0900

    add: 회원 도메인 객체 테스트

---
+++ src/test/java/kr/pravusid/domain/user/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package kr.pravusid.domain.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void 회원을_DB에_저장한다() {
+        User user = new User("tester", "1234", "테스터", "test@kr");
+        userRepository.save(user);
+    }
+
+    @Test
+    public void Username으로_회원을_찾는다() {
+        // WHEN
+        User user = userRepository.findByUsername("tester");
+        // THEN
+        Assert.assertEquals("test@kr", user.getEmail());
+    }
+
+    @Test
+    public void 잘못된_Username은_빈결과를_반환한다() {
+        // WHEN
+        User user = userRepository.findByUsername("admin");
+        // THEN
+        Assert.assertNull(user);
+    }
+
+    @Test
+    public void Email으로_회원을_찾는다() {
+        // WHEN
+        User user = userRepository.findByEmail("test@kr");
+        // THEN
+        Assert.assertEquals("tester", user.getUsername());
+    }
+
+    @Test
+    public void 잘못된_Email은_빈결과를_반환한다() {
+        // WHEN
+        User user = userRepository.findByEmail("admin@kr");
+        // THEN
+        Assert.assertNull(user);
+    }
+
+}
---
+++ src/test/java/kr/pravusid/domain/user/UserTest.java
@@ -0,0 +1,49 @@
+package kr.pravusid.domain.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserTest {
+
+    private User user;
+
+    @Before
+    public void 회원_객체를_생성한다() {
+        user = new User("tester", "1234", "테스터", "test@kr");
+    }
+
+    @Test
+    public void 생성하면_USER_권한을_획득한상태이다() {
+        Assert.assertTrue(user.getAuthorities().contains(Authority.USER));
+    }
+
+    @Test
+    public void 평문_비밀번호를_입력하면_BCrypt_암호화_처리가_되어있다() {
+        // WHEN
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        // THEN
+        Assert.assertTrue(encoder.matches("1234", user.getPassword()));
+    }
+
+    @Test
+    public void 회원_정보를_수정한다() {
+        // GIVEN
+        User origin = new User("tester", "1234", "테스터", "test@kr");
+        User request = new User("new", "4321", "수정", "new@kr");
+        // WHEN
+        origin.update(request);
+        // THEN
+        Assert.assertNotEquals(request.getUsername(), origin.getUsername());
+        Assert.assertEquals(request.getPassword(), origin.getPassword());
+        Assert.assertEquals(request.getName(), origin.getName());
+        Assert.assertEquals(request.getEmail(), origin.getEmail());
+    }
+
+    @Test
+    public void 동일회원인지_확인한다() {
+        Assert.assertTrue(user.verifyUser("tester"));
+    }
+
+}
